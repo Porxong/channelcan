ea2860e0682e64b161a9531a93f2cab591987fe0
Porxong <porxong2019@gmail.com>
2024-09-11 16:51:46 +0700

    update

---
+++ src/app/Services/page.tsx
@@ -0,0 +1,13 @@
+// app/services/page.tsx
+"use strict";
+function Services() {
+    return (
+      <>
+        <h1>Services Page</h1>
+        <p>This is the Services page where you can find information about our services.</p>
+      </>
+    );
+  }
+  
+  export default Services;
+  
